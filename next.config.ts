0e122c9575e63cecf6bf1020be2cb048ab20ee4d
Aycodez <aycom595@gmail.com>
2026-09-22 11:28:58 +0100

    feat: update Next.js configuration to include TypeScript settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  trailingSlash: true,
   images: {
     remotePatterns: [
       {
